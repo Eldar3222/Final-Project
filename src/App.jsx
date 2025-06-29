1c4b2f71bfe0939742144596804238304aec69de
Eldar <ogentum@gmail.com>
2025-06-29 15:15:17 +0400

    Added rectangular icons to search buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Courselist from "./pages/Courselist";
 import Footer from "./componnents/commons/Footer";
 
 export default function App() {
   return (
     <div>
+      <Courselist/>
   
    <Footer/>
     </div>
